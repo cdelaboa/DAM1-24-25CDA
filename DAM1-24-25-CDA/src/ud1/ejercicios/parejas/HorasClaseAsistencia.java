6e10e1c2ab51b8414807dd8872416de439832e78
cdelaboa <cdelaboa@ieschandomonte.edu.es>
2024-10-09 14:17:33 +0200

    nuevos ejercicios

---
+++ DAM1-24-25-CDA/src/ud1/ejercicios/parejas/HorasClaseAsistencia.java
@@ -0,0 +1,68 @@
+package ud1.ejercicios.parejas;
+import java.util.Scanner;
+public class HorasClaseAsistencia {
+    
+    public static void main(String[] args) {
+    //Driver: Carlos de la Torre Aboal
+    //Copilot: David Besada Ramilo
+    
+    Scanner scanner = new Scanner(System.in);
+    final String prog = "Programación";
+    final double tiempoProg = 240;
+
+
+    double min = tiempoProg * 60;
+    double sesiones = min / 50;
+    
+    System.out.println(" A lo largo del año habrá un total de  " + sesiones + " de sesiones " + prog);
+
+
+    //2. número de faltas sin justificar que implican un apercibimiento (>6%)
+
+     boolean  faltasApercibimiento = sesiones * 0.6;
+
+
+
+
+
+
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    scanner.close();;
+
+    }
+}
