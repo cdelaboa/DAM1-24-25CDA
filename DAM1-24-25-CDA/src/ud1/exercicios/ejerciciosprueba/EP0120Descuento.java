aa323399903873b06844fc6dbb1e3a1c238a2674
cdelaboa <cdelaboa@ieschandomonte.edu.es>
2024-10-16 11:08:44 +0200

    des

---
+++ DAM1-24-25-CDA/src/ud1/exercicios/ejerciciosprueba/EP0120Descuento.java
@@ -0,0 +1,28 @@
+package ud1.exercicios.ejerciciosprueba;
+
+import java.util.Scanner;
+
+public class EP0120Descuento {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int niños;
+    int adultos;
+    final double entradaInfantil = 15.50;
+    final double entradaAdulto = 20;
+
+    System.out.println(" Dime cuántas niños sois... ");
+    niños = sc.nextInt();
+
+    System.out.println(" Dime cuántos adultos sois... ");
+    adultos = sc.nextInt();
+
+    sc.close();
+
+    double importeFinal = (adultos * entradaAdulto) + (niños * entradaInfantil);
+
+    double descuento5 = (importeFinal <= 100) ? importeFinal : (importeFinal - (importeFinal * 0.05));
+
+    System.out.println(" El precio final para entrar es de.." + descuento5 + "euros");
+}
+}
