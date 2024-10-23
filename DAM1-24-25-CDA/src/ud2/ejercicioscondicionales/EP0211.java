215df771b7cf980c025d81501faf41a07d91f034
cdelaboa <cdelaboa@ieschadomonte.edu.es>
2024-10-23 20:49:13 +0200

    eg

--- DAM1-24-25-CDA/src/ud2/ejercicioscondicionales/EP0211.java
+++ DAM1-24-25-CDA/src/ud2/ejercicioscondicionales/EP0211.java
@@ -3,7 +3,7 @@ package ud2.ejercicioscondicionales;
 import java.util.*;
 
 public class EP0211 {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         /*
@@ -11,5 +11,23 @@ public class EP0211 {
          * y
          * 9999. La aplicación tendrá que indicar si el número introducido es capicúa.
          */
+
+         System.out.println(" Introduce un número");
+         int num = sc.nextInt();
+         sc.close();
+
+         System.out.println(" Vamos a ver si es capicúa");
+
+         if (esCapicua(num)) {
+            System.out.println("El número " + num + " es capicúa.");
+        } else {
+            System.out.println("El número " + num + " no es capicúa.");
+        }
+
+    // Método para comprobar si un número es capicúa
+    public static boolean esCapicua(int numero) {
+        String numeroStr = Integer.toString(numero);
+        String reversoStr = new StringBuilder(numeroStr).reverse().toString();
+        return numeroStr.equals(reversoStr);
     }
 }
---
+++ DAM1-24-25-CDA/src/ud2/ejercicioscondicionales/EP0212.java
@@ -0,0 +1,93 @@
+package ud2.ejercicioscondicionales;
+
+import java.util.*;
+
+public class EP0212 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Con este programa acertaré la letra de tu DNI solamente diciéndome el tu número...");
+        System.out.println("Entonces dime cual es tu número de DNI");
+        int numDNI = sc.nextInt();
+
+        int letra = numDNI % 23;
+
+        switch (letra) {
+            case 0:
+                System.out.println("La letra es la T");
+                break;
+            case 1:
+                System.out.println("La letra es la R");
+                break;
+            case 2:
+                System.out.println(" Entonces, la letra es la W");
+                break;
+            case 3:
+                System.out.println(" La letra es la A");
+                break;
+            case 4:
+                System.out.println(" La letra es la G");
+                break;
+            case 5:
+                System.out.println(" La letra es la M");
+                break;
+            case 6:
+                System.out.println(" La letra es la Y");
+                break;
+            case 7:
+                System.out.println(" La letra es la F");
+                break;
+            case 8:
+                System.out.println(" La letra es la P");
+                break;
+            case 9:
+                System.out.println(" La letra es la D");
+                break;
+            case 10:
+                System.out.println(" La letra es la X");
+                break;
+            case 11:
+                System.out.println(" La letra es la B");
+                break;
+            case 12:
+                System.out.println(" La letra es la N");
+                break;
+            case 13:
+                System.out.println(" La letra es la J");
+                break;
+            case 14:
+                System.out.println(" La letra es la Z");
+                break;
+            case 15:
+                System.out.println(" La letra es la S");
+                break;
+            case 16:
+                System.out.println(" La letra es la Q");
+                break;
+            case 17:
+                System.out.println(" La letra es la V");
+                break;
+            case 18:
+                System.out.println(" La letra es la H");
+                break;
+            case 19:
+                System.out.println(" La letra es la L");
+                break;
+            case 20:
+                System.out.println(" La letra es la C");
+                break;
+            case 21:
+                System.out.println(" La letra es la K");
+                break;
+            case 22:
+                System.out.println(" La letra es la E");
+                break;
+            default:
+                System.out.println(" No válido");
+                break;
+        }
+        System.out.println(" He acertado?");
+
+    }
+
+}
