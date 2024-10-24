a4f8ffada4aa65da90a8eb183c66209f60f2dedc
cdelaboa <cdelaboa@ieschadomonte.edu.es>
2024-10-24 16:43:52 +0200

    gr

---
+++ DAM1-24-25-CDA/src/ud2/ejercicioscondicionales/EP0215.java
@@ -0,0 +1,26 @@
+package ud2.ejercicioscondicionales;
+import java.util.*;
+public class EP0215 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int largo, ancho;
+
+
+
+        //PROCESO
+        int area;
+        switch (area) {}
+            if(largo <0 && ancho<0)
+            System.out.println("No es válido");
+            break;
+        }
+
+            if(largo>0 && ancho>0)
+            area = largo * ancho;
+            System.out.println("El área es de: " + area);
+           
+            System.out.println("n");
+                break;
+        }
+}
+}
