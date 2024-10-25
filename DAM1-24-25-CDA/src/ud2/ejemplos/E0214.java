c09bea35a2f4ed59fb5dc1f69119e045759de6e7
cdelaboa <cdelaboa@ieschandomonte.edu.es>
2024-10-25 13:55:34 +0200

    olp

--- DAM1-24-25-CDA/src/ud2/ejemplos/E0214.java
+++ DAM1-24-25-CDA/src/ud2/ejemplos/E0214.java
@@ -9,22 +9,51 @@ public class E0214 {
 
          int dia, mes, anho;
          System.out.println(" Dime la fecha del año que quieras ( especificando día, mes y año)");
+         System.out.println("DÍA: ");
          dia = sc.nextInt();
+         System.out.println("MES: ");
          mes = sc.nextInt();
+        System.out.println("AÑO: ");
          anho = sc.nextInt();
          sc.close();
 
-         System.out.println("Es el "+ dia + "de" + mes + "del" + anho);
+         System.out.println("Es el "+ dia + " del " + mes + " del " + anho);
 
          System.out.println("Vamos a añadirle un día");
-         dia = dia + 1;
+         dia++;
 
-         if (dia == 30) {
-            dia = 1;
-            mes = mes + 1;
+         switch (mes) {
+            case 1,3,5,7,8,10,12:
+                if (dia > 31) {
+                    dia = 1;
+                    mes++;
+                }
+                break;
+            case 2:
+                if (dia > 28) {
+                    dia = 1;
+                    mes++;
+                }
+                break;
+
+            case 4,6,9,11:
+                if(dia> 30) {
+                    dia = 1;
+                    mes++;
+                }
+                break;
+           
          }
+         
+         if (mes > 12) {
+            mes = 1;
+            anho++;
+         }
+
         
-         System.out.printf("Es el %dde%ddel%d", dia, mes, anho);
+         
+        
+         System.out.printf("Es el %d del %d del %d", dia, mes, anho);
 
     }
 }
