0ec37edffba7575724193d01a9307652e9d21290
cdelaboa <cdelaboa@ieschandomonte.edu.es>
2024-11-06 13:32:44 +0100

    + ejer

---
+++ DAM1-24-25-CDA/src/ud2/ejerciciosbucles/ejerciciospropuestos/EP0311_Decimal_Binario.java
@@ -0,0 +1,16 @@
+package ud2.ejerciciosbucles.ejerciciospropuestos;
+import java.util.*;
+public class EP0311_Decimal_Binario {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    /* Realiza un programa que convierta un número entero decimal
+    en su representación binaria. Hay que tener en cuenta que desconocemos cuántas cifras
+    tiene el número que introduce el usuario.
+    Por simplicidad, iremos mostrando el número binario con un digito por línea. */
+
+
+    System.out.println("Dime un número: ");
+    int num = sc.nextInt();
+    
+}
+}
