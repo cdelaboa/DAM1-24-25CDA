d9d0a7180710f92f80088ed8df9ef75dc13ab0a3
cdelaboa <cdelaboa@ieschandomonte.edu.es>
2024-11-15 14:27:05 +0100

    ejer 415

---
+++ DAM1-24-25-CDA/src/ud2/ejerciciosfunciones/EP0415.java
@@ -0,0 +1,8 @@
+package ud2.ejerciciosfunciones;
+//CARLOS DE LA TORRE ABOAL
+public class EP0415 {
+    public static int diferenciaMin (int hora1, int minuto1, int hora2, int minuto2) {
+        
+    }
+}
+}
