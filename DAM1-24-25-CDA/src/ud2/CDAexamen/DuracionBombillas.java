e5dd4d96e3ad28b91f60668b8f3928dbc2e184af
cdelaboa <cdelaboa@ieschandomonte.edu.es>
2024-11-21 11:22:15 +0100

    borrar librerias

--- DAM1-24-25-CDA/src/ud2/CDAexamen/DuracionBombillas.java
+++ DAM1-24-25-CDA/src/ud2/CDAexamen/DuracionBombillas.java
@@ -1,5 +1,6 @@
 package ud2.CDAexamen;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
 // CARLOS DE LA TORRE ABOAL
 public class DuracionBombillas {
 
@@ -23,6 +24,22 @@ public class DuracionBombillas {
         }
         return causaFinBombilla;
 
+    }
+
+    @Test
+    public void causaFinBombillaTest() {
+        assertEquals("ERROR", causaFinBombilla(100, 100, 100));
+        assertEquals("ERROR", causaFinBombilla(1000, -10, 10));
+        assertEquals("ERROR", causaFinBombilla(0, 10, 10));
+        assertEquals("ERROR", causaFinBombilla(1000, 10, 0));
+        assertEquals("ENCENDIDOS", causaFinBombilla(500, 400, 1));
+        assertEquals("HORAS", causaFinBombilla(1000, 10000, 1));
+        assertEquals("HORAS", causaFinBombilla(1000, 200, 10));
+        assertEquals("HORAS", causaFinBombilla(2000, 700, 3));
+        assertEquals("ENCENDIDOS", causaFinBombilla(2000, 600, 3));
+        assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 1));
+        assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 9));
+        assertEquals("ENCENDIDOS + HORAS", causaFinBombilla(1000, 100, 10));
     }
 
 }
